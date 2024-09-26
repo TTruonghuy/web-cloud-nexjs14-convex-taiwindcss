562e976a3fc0c6216f7d109bd8b865e2e45cb1fa
TTruonghuy <ttruonghuy2003@gmail.com>
2024-09-26 16:38:30 +0700

    gan xong roi

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,17 +1,40 @@
  "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+// import { ConvexProvider, ConvexReactClient } from "convex/react";
+// import { ReactNode } from "react";
+// import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+// export function ConvexClientProvider({ children }: { children: ReactNode }) {
+//   return (
+//   <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+//       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+//        {children}
+//       </ConvexProviderWithClerk>
+//     </ClerkProvider>
+//   );
+// }
+
 import { ReactNode } from "react";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-       {children}
+        {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
